a67d9f82e416a3e3622a70f44c892d20267ad20c
raccoon2389 <roy0998@gmail.com>
2020-05-29 19:53:28 +0900

    .

--- main copy.py
+++ main copy.py
@@ -18,13 +18,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import missingno as msno
+from keras.callbacks import EarlyStopping,ModelCheckpoint
+e_stop = EarlyStopping(monitor='loss',patience=5,mode='auto')
+
+m_check = ModelCheckpoint(filepath=".\keras_prac\model\{epoch:02d}--{val_loss:.4f}.hdf5", monitor = 'val_loss',save_best_only=True)
 
 train = pd.read_csv('data/train.csv',index_col=0)
 test = pd.read_csv('data/test.csv', index_col=0)
 submission = pd.read_csv('data/sample_submission.csv')
 
-train_dst = train.filter(regex='_dst$', axis=1).replace(0, np.NaN) # dst 데이터만 따로 뺀다.
-test_dst = test.filter(regex='_dst$', axis=1).replace(0, np.NaN) # 보간을 하기위해 결측값을 삭제한다.
+train_dst = train.filter(regex='_dst$', axis=1)
+test_dst = test.filter(regex='_dst$', axis=1)
 test_dst.head(1)
 
 # print(train_dst)
@@ -36,34 +40,29 @@ test_dst = test_dst.interpolate(methods='linear', axis=1)
 
 train_dst.fillna(0, inplace=True) 
 test_dst.fillna(0, inplace=True)
-print(test_dst.head())
 
+# print(test_dst.head())
+# 
 train.update(train_dst) # 보간한 데이터를 기존 데이터프레임에 업데이트 한다.
 test.update(test_dst)
-# df = pd.DataFrame()
-# d = train['rho']
+# 
 
-# for i in range(650,1000,10):
 
-#     train.loc[train[f"{i}_src"]<0.1,f"{i}_src"]=1e-4
-#     df[f"{i}_dst"] = train[f"{i}_dst"]
-#     df[f"{i}_src"] = train[f"{i}_src"]
-
-# print(df.head())
-
-sclar = StandardScaler()
-sclar.fit(train)
-df = sclar.transform(train)
 # print(df.shape)
 # col = range(650,1000,10)
 
 # x_train = df
 
-# train.filter(regex='_src$',axis=1).head().T.plot()
-# plt.figure(figsize=(4,12))test.filter(regex='_src$',axis=1).head().T.plot()
+#데이터 분석
+
+# train.filter(regex='_src$',axis=1)[16:20].T.plot()
+# plt.figure(figsize=(4,12))
+# train.filter(regex='_dst$',axis=1)[16:20].T.plot()
 # sns.heatmap(train.corr().loc['rho':'990_dst','hhb':].abs())
-# sns.heatmap(train.corr().loc['rho':'990_src','650_dst':'990_dst'].abs())
+# sns.heatmap(train.corr().loc['650_dst':'990_dst','650_src':'990_src'].abs())
+# sns.heatmap(train.corr().loc['650_src':'990_src','650_dst':'990_dst'].abs().plot())
 # plt.show()
+
 
 
 # print(train.head())
@@ -81,42 +80,26 @@ df = sclar.transform(train)
 # print(test_dst.head())
 
 
-
-x_train = train.loc[:,"650_dst":"990_dst"]
+x_train = train.loc[:,'rho':'990_dst']
 y_train = train.loc[:,'hhb':]
+sclar = StandardScaler()
+sclar.fit(x_train)
+x_train = sclar.transform(x_train)
+test =sclar.transform(test)
 
 # print(x_train.shape, y_train.shape)
 skf = KFold(n_splits=5,shuffle=True)
 accuracy = []
 for t, v in skf.split(x_train):
     print(t)
-    x_t , y_t = x_train.iloc[t], y_train.iloc[t]
+    x_t , y_t = x_train[t], y_train.iloc[t]
 
-    input_rho = Input(shape=(1,))
-    input_src = Input(shape=(35,))
-    input_dst = Input(shape=(35,))
+    model = Sequential()
+    model.add(Dense(10000,input_shape=(71,),activation='relu'))
 
-    dense_rho_1 = Dense(1, activation = 'relu')(input_rho)
-
-    dense_src_1 = Dense(100, activation = 'relu')(input_src)
-    dense_src_1 = Dropout(0.2)(dense_src_1)
-
-    dense_src_2 = Dense(100, activation = 'relu')(dense_src_1)
-    dense_src_2 = Dropout(0.2)(dense_src_2)
-    dense_src_3 = Dense(100, activation = 'relu')(dense_src_2)
-    dense_src_3 = Dropout(0.2)(dense_src_3)
-
-    dense_dst_1 = Dense(100, activation = 'relu')(input_dst)
-    dense_dst_1 = Dropout(0.2)(dense_dst_1)
-
-    dense_dst_2 = Dense(100, activation = 'relu')(dense_dst_1)
-    dense_dst_2 = Dropout(0.2)(dense_dst_2)
-
-    dense_dst_3 = Dense(100, activation = 'relu')(dense_dst_2)
-    dense_dst_3 = Dropout(0.2)(dense_dst_3)
-
-    
-    # model.add(Dense(50,activation='relu'))
+    model.add(Dense(1000,activation='relu'))
+    model.add(Dense(1000,activation='relu'))
+    model.add(Dense(50,activation='relu'))
     model.add(Dense(4))
 
     
